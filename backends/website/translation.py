c54700e3ac9d01cb437981e82f3dce5e760516c8
Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
2019-05-21 15:40:47 +0200

    First step in refactoring website into a backend
    The idea is to use flask-frozen to generate a static snapshot of the
    webpage for hosting.
    
    This first commit moves files around, but a lot of functionality is
    defunct or deactivated.

---
+++ backends/website/translation.py
@@ -0,0 +1,16 @@
+from flask.ext.babelex import Domain
+from os import environ,listdir
+from os.path import join
+
+trans_dir = join("/home/jreinhardt/Models/BOLTS","translations")
+messages_domain = Domain(trans_dir,domain="messages")
+parts_domain = Domain(trans_dir,domain="parts")
+docs_domain = Domain(trans_dir,domain="docs")
+
+languages = listdir(trans_dir) + ['en']
+
+def gettext_parts(msgid):
+	return parts_domain.gettext(msgid)
+
+def gettext_docs(msgid):
+	return docs_domain.gettext(msgid)
